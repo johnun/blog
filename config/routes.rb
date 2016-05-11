441f755cb753e4ca1d20dd930527f5cbd8f37f3a
johnun <john@johnun.com>
2016-05-11 20:59:49 +0800

    Clean up homepage and labels

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :contacts, only: [:new, :create]
   get 'welcome/index'
-  root 'welcome/index'
+  root 'welcome#index'
 
   get '*path' => redirect('/')
 end
