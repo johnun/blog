5bfab0d15c2d586352e0c06ab9250d854a65a3d2
johnun <john@johnun.com>
2016-05-14 15:17:04 +0800

    Add logo and changed colours

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :projects
   resources :contacts, only: [:new, :create]
-  get 'welcome/index'
   root 'welcome#index'
 
   get '*path' => redirect('/')
