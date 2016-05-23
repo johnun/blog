2d63e2ba479858bc6adebbc80a1f25c278f64c74
johnun <john@johnun.com>
2016-05-23 22:41:56 +0800

    Fix previous page link

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,12 @@ class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  def prev
+    Post.where("id < ?", id).limit(1).last
+  end
+  
   def next
     Post.where("id > ?", id).limit(1).first
-  end
-
-  def prev
-    Post.where("id < ?", id).limit(1).first
   end
 end
 
