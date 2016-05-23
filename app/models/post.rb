61e7ed5dc4cdffac5042cd6be8dec824207967ab
johnun <john@johnun.com>
2016-05-23 22:21:26 +0800

    Add previous page and next page links to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  def next
+    Post.where("id > ?", id).limit(1).first
+  end
+
+  def prev
+    Post.where("id < ?", id).limit(1).first
+  end
 end
 
 
